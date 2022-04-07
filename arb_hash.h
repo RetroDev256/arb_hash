71101be46cb6a48268b7160f710e33965d0e3e43
Eric Petersen <ericpetersen3.14@gmail.com>
2022-04-07 10:26:29 +0100

    Updated conditional checks

--- arb_hash.h
+++ arb_hash.h
@@ -10,27 +10,28 @@ typedef struct ArbHashBlock {
 
 static inline void arb_hash(const AHBlock in, AHBlock out, const uint64_t rounds);
 static inline void arb_hash(const AHBlock in, AHBlock out, const uint64_t rounds) {
+    if (!rounds || !in.data || !out.data || out.data == in.data || out.count < in.count || in.count < 3) {
+        return;
+    }
     const uint_fast8_t shifts[5] = {1, 2, 3, 5, 7};
-    if (in.data && out.data && out.count >= in.count && in.count >= 3) {
-        for (uint_fast64_t i = 0; i < in.count; i++) {
-            out.data[i] = in.data[i];
-        }
-        for (uint_fast64_t rnd = 0; rnd < rounds; rnd++) {
-            for (uint_fast64_t bshft = 0; bshft < in.count; bshft++) {
-                for (uint_fast64_t elem = 0; elem < in.count; elem++) {
-                    const uint_fast8_t s = shifts[(bshft + elem) % 5];
-                    uint8_t *const restrict a = out.data + elem;
-                    uint8_t *const restrict b = out.data + (elem + 1) % in.count;
-                    uint8_t *const restrict c = out.data + (elem + 2) % in.count;
-                    *a += *b;
-                    *c ^= *a;
-                    *c = (uint8_t)((*c << s) | (*c >> (8 - s)));
-                }
+    for (uint_fast64_t i = 0; i < in.count; i++) {
+        out.data[i] = in.data[i];
+    }
+    for (uint_fast64_t rnd = 0; rnd < rounds; rnd++) {
+        for (uint_fast64_t bshft = 0; bshft < in.count; bshft++) {
+            for (uint_fast64_t elem = 0; elem < in.count; elem++) {
+                const uint_fast8_t s = shifts[(bshft + elem) % 5];
+                uint8_t *const restrict a = out.data + elem;
+                uint8_t *const restrict b = out.data + (elem + 1) % in.count;
+                uint8_t *const restrict c = out.data + (elem + 2) % in.count;
+                *a += *b;
+                *c ^= *a;
+                *c = (uint8_t)((*c << s) | (*c >> (8 - s)));
             }
         }
-        for (uint_fast64_t i = 0; i < in.count; i++) {
-            out.data[i] ^= in.data[i];
-        }
+    }
+    for (uint_fast64_t i = 0; i < in.count; i++) {
+        out.data[i] ^= in.data[i];
     }
 }
 
